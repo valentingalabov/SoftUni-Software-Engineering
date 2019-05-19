8df3059f092c1852470b6016ce0c50a18580831e
Valentin Galabov <50594546+desireous@users.noreply.github.com>
2019-05-19 12:40:37 +0300

    Multidimensional Arrays

---
+++ C# - Advanced/02. MULTIDIMENSIONAL ARRAYS/MULTIDIMENSIONAL ARRAYS - Lab/03. Primary Diagonal/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[sizeOfMatrix, sizeOfMatrix];
+            int sum = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] tokens = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = tokens[j];
+                    if (i == j)
+                    {
+                        sum += matrix[i, j];
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ C# - Advanced/02. MULTIDIMENSIONAL ARRAYS/MULTIDIMENSIONAL ARRAYS - Lab/04. Symbol in Matrix/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Symbol_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[sizeOfMatrix,sizeOfMatrix];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                string input = Console.ReadLine();
+                for (int j = 0; j < input.Length; j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+            }
+            char symbolToFind = char.Parse(Console.ReadLine());
+            int rowPosition = 0;
+            int colPosition = 0;
+            bool foundIt = false;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row,col]==symbolToFind)
+                    {
+                        rowPosition = row;
+                        colPosition = col;
+                        foundIt = true;
+                        break;
+                    }
+                    
+                }
+                if (foundIt)
+                {
+                    break;
+                }
+            }
+            if (!foundIt)
+            {
+                Console.WriteLine($"{symbolToFind} does not occur in the matrix");
+            }
+            else
+            {
+                Console.WriteLine($"({rowPosition}, {colPosition})");
+            }
+            
+        }
+    }
+}
