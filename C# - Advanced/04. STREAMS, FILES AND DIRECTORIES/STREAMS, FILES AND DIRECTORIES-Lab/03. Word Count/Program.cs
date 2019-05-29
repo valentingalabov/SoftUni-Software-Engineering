be836492948863c3b7cbaee12a7f7cc51ef3bf42
Valentin Galabov <50594546+desireous@users.noreply.github.com>
2019-05-29 12:38:11 +0300

    Added lab exercise

--- C# - Advanced/04. STREAMS, FILES AND DIRECTORIES/STREAMS, FILES AND DIRECTORIES-Lab/03. Word Count/Program.cs
+++ C# - Advanced/04. STREAMS, FILES AND DIRECTORIES/STREAMS, FILES AND DIRECTORIES-Lab/03. Word Count/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace _03._Word_Count
 {
@@ -6,7 +9,59 @@ namespace _03._Word_Count
     {
         static void Main(string[] args)
         {
-            
+            string pathFolder = "files";
+            string textFile = "text.txt";
+            string wordsFile = "words.txt";
+
+            Dictionary<string, int> wordsCount = new Dictionary<string, int>();
+
+            using (var readerForWords = new StreamReader(Path.Combine(pathFolder, wordsFile)))
+            {
+                string[] line = readerForWords.ReadLine().Split(" ").ToArray();
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (!wordsCount.ContainsKey(line[i].ToLower()))
+                    {
+                        wordsCount.Add(line[i].ToLower(), 0);
+                    }
+
+                }
+            }
+            using (var readerForText = new StreamReader(Path.Combine(pathFolder, textFile)))
+            {
+                char[] separators = { ' ', ',', '-', '.' };
+                string text = readerForText.ReadLine();
+                while (text != null)
+                {
+                    string[] currLine = text.Split(separators).ToArray();
+                    for (int i = 0; i < currLine.Length; i++)
+                    {
+                        string currWord = currLine[i].ToLower();
+
+                        if (wordsCount.ContainsKey(currWord))
+                        {
+                            wordsCount[currWord]++;
+                        }
+
+
+                    }
+                    text = readerForText.ReadLine();
+                }
+
+               
+            }
+            string output = "Output.txt";
+            using (var writer = new StreamWriter(Path.Combine(pathFolder, output)))
+            {
+                foreach (var kvp in wordsCount.OrderByDescending(x => x.Value))
+                {
+                    writer.WriteLine($"{kvp.Key} - {kvp.Value}");
+                }
+
+
+            }
+
+
 
         }
     }
---
+++ C# - Advanced/04. STREAMS, FILES AND DIRECTORIES/STREAMS, FILES AND DIRECTORIES-Lab/04. Merge Files/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _04._Merge_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathFolder = "Files";
+            string firstFile = "FileOne.txt";
+            string secondFile = "FileTwo.txt";
+            string outputFile = "Output.txt";
+            List<string> result1 = new List<string>();
+            List<string> result2 = new List<string>();
+
+            using (var readerFirsFile = new StreamReader(Path.Combine(pathFolder, firstFile)))
+            {
+                string currLine = readerFirsFile.ReadLine();
+                while (currLine != null)
+                {
+                    result1.Add(currLine);
+                    currLine = readerFirsFile.ReadLine();
+                }
+
+            }
+            using (var readerSecondFile = new StreamReader(Path.Combine(pathFolder, secondFile)))
+            {
+                string currLine = readerSecondFile.ReadLine();
+
+                while (currLine!= null)
+                {
+                    result2.Add(currLine);
+                    currLine = readerSecondFile.ReadLine();
+                }
+               
+            }
+
+            using (var writer=new StreamWriter(Path.Combine(pathFolder,outputFile)))
+            {
+                for (int i = 0; i < result1.Count; i++)
+                {
+                    writer.WriteLine(result1[i]);
+                    writer.WriteLine(result2[i]);
+                }
+            }
+
+        }
+    }
+}
