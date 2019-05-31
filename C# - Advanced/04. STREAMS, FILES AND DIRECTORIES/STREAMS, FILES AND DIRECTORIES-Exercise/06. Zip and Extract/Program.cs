39b3bcd4231db3a20e3548b64914a00e3a0b9fa4
Valentin Galabov <50594546+desireous@users.noreply.github.com>
2019-05-31 17:15:43 +0300

    Stream Files And Directories

---
+++ C# - Advanced/04. STREAMS, FILES AND DIRECTORIES/STREAMS, FILES AND DIRECTORIES-Exercise/06. Zip and Extract/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace _06._Zip_and_Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            var zipFile = @"..\..\..\Result.zip";
+            var file = "copyMe.png";
+
+            using (var archive = ZipFile.Open(zipFile, ZipArchiveMode.Create))
+            {
+                
+                    archive.CreateEntryFromFile(file, Path.GetFileName(file));
+                
+            }
+        }
+
+    }
+}
+
