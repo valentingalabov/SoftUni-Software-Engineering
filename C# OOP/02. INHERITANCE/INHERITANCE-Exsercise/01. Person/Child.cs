4bbbb6ef35939a463eb4f1e0c2963592967ed8e3
Valentin Galabov <50594546+desireous@users.noreply.github.com>
2019-07-04 17:06:27 +0300

    exercise 5/5

---
+++ C# OOP/02. INHERITANCE/INHERITANCE-Exsercise/01. Person/Child.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age)
+            : base(name, age)
+        {
+
+        }
+
+       
+        
+    }
+}
---
+++ C# OOP/02. INHERITANCE/INHERITANCE-Exsercise/05. Restaurant/Cake.cs
@@ -0,0 +1,17 @@
+﻿namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const decimal CakePrice = 5m;
+
+        private const double CakeGrams = 250;
+
+        private const double CakeCalories = 1000;
+
+        public Cake(string name) 
+            : base(name, CakePrice, CakeGrams, CakeCalories)
+        {
+
+        }
+    }
+}
