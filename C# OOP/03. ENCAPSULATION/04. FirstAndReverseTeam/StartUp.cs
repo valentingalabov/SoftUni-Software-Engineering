b9d035b6324ed78ab9990edf334c7e35bde46f13
Valentin Galabov <50594546+desireous@users.noreply.github.com>
2019-07-06 21:15:40 +0300

    Encapsulation

---
+++ C# OOP/03. ENCAPSULATION/04. FirstAndReverseTeam/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+        
+            int n = int.Parse(Console.ReadLine());
+
+            while (n-- > 0)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split();
+
+                Person p = new Person(tokens[0], tokens[1], int.Parse(tokens[2]), decimal.Parse(tokens[3]));
+                people.Add(p);
+            }
+
+            Team team = new Team("SoftUni");
+
+            foreach (Person person in people)
+            {
+                team.AddPlayer(person);
+
+            }
+
+  
+
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+
+        }
+    }
+}
