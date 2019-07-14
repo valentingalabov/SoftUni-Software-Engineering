954fd383ed486d1a3c8f29c33590d4a988859e8b
Valentin Galabov <50594546+desireous@users.noreply.github.com>
2019-07-14 11:27:58 +0300

    Polymorphism

---
+++ C# OOP/05. POLYMORPHISM/POLYMORPHISM-Lab/03. Shapes/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius { get; set; }
+
+
+
+        public override double CalculateArea()
+        {
+            return Math.PI * this.Radius * this.Radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * this.Radius;
+        }
+
+        public sealed override string Draw()
+        {
+            return base.Draw() + "Circle";
+        }
+    }
+}
---
+++ C# OOP/05. POLYMORPHISM/POLYMORPHISM-Lab/03. Shapes/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(double height, double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public double Height { get; private set; }
+
+        public double Width { get; set; }
+
+
+
+        public override double CalculateArea()
+        {
+            return this.Width * this.Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return this.Height * 2 + this.Width * 2;
+        }
+
+        public sealed override string Draw()
+        {
+            return base.Draw() + "Rectangle";
+        }
+    }
+}
