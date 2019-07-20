ca7d15af80a86ba9a05ae62e60e3d643763b4bb2
Valentin Galabov <50594546+desireous@users.noreply.github.com>
2019-07-20 22:58:50 +0300

    Add app

---
+++ Remove all Obj,Bin folders and zip files From solutions/DeleteMyTrashSolutionFiles/DeleteTrashItems.cs
@@ -0,0 +1,58 @@
+﻿
+using System.IO;
+using System.Linq;
+
+namespace DeleteAllBinObjAndZipFilesFromMySolution
+{
+    public class DeleteTrashItems
+    {
+        
+
+        public void DeleteUnusedItems(string path)
+        {
+            var directoryToOpperate = new DirectoryInfo(path);
+
+            var directoriesOfBinFolders = directoryToOpperate.GetDirectories("bin", SearchOption.AllDirectories);
+            var directoriesOfObjFolders = directoryToOpperate.GetDirectories("obj", SearchOption.AllDirectories);
+
+            var directoriesOfAllSolutions = directoryToOpperate.GetDirectories();
+            foreach (var folder in directoriesOfAllSolutions)
+            {
+                folder.EnumerateFiles().Where(x => x.Extension == ".zip")
+                     .ToList().ForEach(f => f.Delete());
+
+                folder.EnumerateFiles().Where(x => x.Extension == ".rar")
+                     .ToList().ForEach(f => f.Delete());
+            }
+
+
+            foreach (var itemsInBin in directoriesOfBinFolders)
+            {
+                itemsInBin.EnumerateDirectories()
+                  .ToList().ForEach(d => d.Delete(true));
+
+                itemsInBin.EnumerateFiles()
+                    .ToList().ForEach(f => f.Delete());
+
+
+
+                itemsInBin.Delete();
+            }
+            foreach (var itemsInObj in directoriesOfObjFolders)
+            {
+                itemsInObj.EnumerateDirectories()
+                    .ToList().ForEach(d => d.Delete(true));
+                itemsInObj.EnumerateFiles()
+                    .ToList().ForEach(f => f.Delete());
+
+
+
+                itemsInObj.Delete();
+
+            }
+
+            System.Console.WriteLine("All files are deleted!");
+        }
+
+    }
+}
---
+++ Remove all Obj,Bin folders and zip files From solutions/DeleteMyTrashSolutionFiles/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DeleteAllBinObjAndZipFilesFromMySolution
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"C:\Users\Valio\Desktop\Soft Uni - Software Engineering";
+
+            DeleteTrashItems delete = new DeleteTrashItems();
+            delete.DeleteUnusedItems(path);
+        }
+    }
+}
