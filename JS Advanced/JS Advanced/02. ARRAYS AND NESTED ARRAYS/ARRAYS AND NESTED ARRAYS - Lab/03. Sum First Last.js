0215a4856ead62f254569f54f96c14049d4f2632
v_galabow@abv.bg <v_galabow@abv.bg>
2021-02-01 23:51:43 +0200

    Implement new folder

--- JS Advanced/JS Advanced/02. ARRAYS AND NESTED ARRAYS/ARRAYS AND NESTED ARRAYS - Lab/03. Sum First Last.js
+++
@@ -1,9 +0,0 @@
-function solve(inputArray) {
-let firsElement = Number(inputArray[0]);
-let secondElement = Number(inputArray[inputArray.length - 1]);
-
-return firsElement + secondElement;
-}
-
-console.log(solve(['20', '30', '40']));
-console.log(solve(['5', '10']));
--- JS Advanced/JS Advanced/02. ARRAYS AND NESTED ARRAYS/ARRAYS AND NESTED ARRAYS - Lab/06. Bigger Half.js
+++
@@ -1,9 +0,0 @@
-function solve(inputArray) {
-  return inputArray
-        .sort((a, b) => a - b)
-        .slice(inputArray.length / 2);
-}
-
-
-console.log(solve([4, 7, 2, 5]));
-console.log(solve([3, 19, 14, 7, 2, 19, 6]));
