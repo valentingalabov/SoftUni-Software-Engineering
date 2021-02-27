96c719993604e30db3c03d27782354dbb1b1b309
v_galabow@abv.bg <v_galabow@abv.bg>
2021-02-27 23:00:06 +0200

    Advanced Functions

--- JS Advanced/JS Advanced/05. ADVANCED FUNCTIONS/ADVANCED FUNCTIONS - Exercise/08. Next Article/script.js
+++ JS Advanced/JS Advanced/05. ADVANCED FUNCTIONS/ADVANCED FUNCTIONS - Exercise/08. Next Article/script.js
@@ -1,3 +1,21 @@
 function getArticleGenerator(articles) {
-    // TODO
+    let content = document.querySelector('#content');
+    let counter = 0;
+
+    return function showNext() {
+        if (counter > articles.length-1 || articles[counter] == '') {
+            return;
+        }
+        let article = document.createElement("article");
+        article.textContent = articles[counter];
+        article.style.border = "thick solid #0000FF"
+        content.appendChild(article);
+        counter++;
+
+    }
+
+
+
+
 }
+
