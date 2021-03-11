20fbe430765d39649fd8ca29f89a29f98abe954e
v_galabow@abv.bg <v_galabow@abv.bg>
2021-03-11 23:48:36 +0200

    EXAM PREPARATION

---
+++ JS Advanced/JS Advanced/09. EXAM PREPARATION/01. SoftUni Administration/Skeleton/index.js
@@ -0,0 +1,103 @@
+function solve() {
+    const formControls = document.querySelectorAll('.form-control input');
+    const lecture = formControls[0];
+    const date = formControls[1];
+    const moduleName = document.querySelector('select');
+    const modulesOutput = document.querySelector('.modules');
+
+    let state = {};
+
+
+    function createElement(type, text, attributes = []) {
+        let element = document.createElement(type);
+        if (text) {
+            element.textContent = text;
+        }
+
+        attributes
+            .map(attr => attr.split('='))
+            .forEach(([name, value]) => {
+                element.setAttribute(name, value);
+            });
+
+        return element;
+    }
+
+    function add(e) {
+        e.preventDefault();
+
+        if (lecture.value === ''
+            || date.value === ''
+            || moduleName.value === 'Select module') {
+            return;
+        }
+
+
+        let lectureName = lecture.value;
+        let dateString = date.value;
+
+        const lectureTitle = lectureName + ' - ' + dateString.split('-').join('/').split('T').join(' - ');
+
+        const delBtn = createElement('button', 'Del', ['class=red']);
+        const lectureH4 = createElement('h4', lectureTitle);
+        const li = createElement('li', undefined, ['class=flex']);
+
+        li.appendChild(lectureH4);
+        li.appendChild(delBtn);
+
+        let module;
+        let ul;
+
+
+        if (!state[moduleName.value]) {
+            let h3 = createElement('h3', `${moduleName.value.toUpperCase()}-MODULE`);
+            ul = createElement('ul');
+            lis = [];
+            module = createElement('div', undefined, ['class=module']);
+
+            module.appendChild(h3);
+            module.appendChild(ul);
+            state[moduleName.value] = { module, ul, lis: [] };
+        } else {
+            module = state[moduleName.value].module;
+            ul = state[moduleName.value].ul;
+            lis = state[moduleName.value].lis;
+        }
+
+        state[moduleName.value].lis.push({ li, date: date.value });
+
+        state[moduleName.value].lis.sort((a, b) => a.date.localeCompare(b.date)).forEach(({ li }) => {
+            ul.appendChild(li);
+        });
+
+    }
+
+    function del(e) {
+        let li = e.target.parentNode;
+        let ul = li.parentNode;
+        let module = ul.parentNode;
+
+        li.remove();
+        if (ul.children.length === 0) {
+            module.remove();
+        }
+    }
+
+    document.getElementsByTagName('main')[0].addEventListener('click', (e) => {
+
+        if (e.target.tagName === 'BUTTON') {
+            if (!e.target.classList.contains('red')) {
+                add(e);
+
+                Object.entries(state).forEach(([name, module]) => {
+                    modulesOutput.appendChild(module.module);
+                });
+            } else {
+                del(e);
+            }
+
+        }
+
+    });
+
+}
