1210d9de81a96c4d7c1c1c8cebc3a30e61d2849c
v_galabow@abv.bg <v_galabow@abv.bg>
2021-03-29 00:59:40 +0300

    Architecture and testing

---
+++ JS Advanced/JS Applications/04. ARCHITECTURE AND TESTING/ARCHITECTURE AND TESTING - Exercise/03.SoftTerest/src/views/details.js
@@ -0,0 +1,50 @@
+import { getIdeaById, deleteIdeaById } from '../api/data.js';
+import { e } from '../dom.js';
+
+
+function createIdeaCard(idea, isOwner, goTo) {
+    const result = document.createDocumentFragment();
+    result.appendChild(e('img', { className: 'det-img', src: idea.img },));
+    result.appendChild(e('div', { className: 'desc' },
+        e('h2', { className: 'display-5' }, idea.title),
+        e('p', { className: 'infoType' }, 'Description:'),
+        e('p', { className: 'idea-description' }, idea.description)
+    ));
+    if (isOwner) {
+        result.appendChild(e('div', { className: 'text-center' },
+            e('a', { className: 'btn detb', href: '', onClick: onDelete }, 'Delete')
+        ));
+
+    }
+
+    return result;
+
+    async function onDelete(event) {
+        event.preventDefault();
+
+        const confirmed = confirm('Are you shure you want to delete this idea?');
+        if (confirmed) {
+            await deleteIdeaById(idea._id);
+            goTo('dashboard');
+        }
+    }
+}
+
+
+export function setupDetils(section, navigation) {
+    return showDetails;
+
+    async function showDetails(id) {
+        section.innerHTML = '';
+
+        const userId = sessionStorage.getItem('userId');
+        const idea = await getIdeaById(id);
+
+
+        const card = createIdeaCard(idea, userId == idea._ownerId, navigation.goTo);
+
+        section.appendChild(card);
+
+        return section;
+    }
+}
